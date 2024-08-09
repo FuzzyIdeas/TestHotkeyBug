f64ab06d0f2378367ce75763e5079f6924d66cbf
Alin Panaitiu <alin.panaitiu@gmail.com>
2024-08-09 19:02:03 +0300

    Add hotkey test code

--- TestHotkey/TestHotkeyApp.swift
+++ TestHotkey/TestHotkeyApp.swift
@@ -5,10 +5,40 @@
 //  Created by Alin Panaitiu on 09.08.2024.
 //
 
+import Carbon
 import SwiftUI
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let hotKeyId = EventHotKeyID(signature: 0x1234_2324, id: UInt32.random(in: 0 ... UInt32.max))
+        var carbonHotKey: EventHotKeyRef?
+
+        let eventDispatcherError = RegisterEventHotKey(
+            UInt32(kVK_ANSI_A),
+            UInt32(optionKey),
+            hotKeyId,
+            GetEventDispatcherTarget(),
+            0,
+            &carbonHotKey
+        )
+        print("Error with GetEventDispatcherTarget:", eventDispatcherError)
+
+        let applicationEventError = RegisterEventHotKey(
+            UInt32(kVK_ANSI_A),
+            UInt32(optionKey),
+            hotKeyId,
+            GetApplicationEventTarget(),
+            0,
+            &carbonHotKey
+        )
+        print("Error with GetApplicationEventTarget:", applicationEventError)
+    }
+}
 
 @main
 struct TestHotkeyApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
