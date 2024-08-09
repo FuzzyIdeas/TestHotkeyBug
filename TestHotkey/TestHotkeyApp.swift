93496adc55e2b527eadb13219f5813248d3be4d6
Alin Panaitiu <alin.panaitiu@gmail.com>
2024-08-09 18:56:59 +0300

    Initial Commit

---
+++ TestHotkey/TestHotkeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestHotkeyApp.swift
+//  TestHotkey
+//
+//  Created by Alin Panaitiu on 09.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestHotkeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
